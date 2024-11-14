a2fea0a6ef44b3d0535d7dab8beb2cd20745706e
Shashika2001 <ekanayakespm@gmail.com>
2024-11-14 09:25:03 +0530

    create Home page

---
+++ portfolio-react/src/components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+export default function Hero() {
+  return (
+    
+      <div className="hero">
+        <img src={profile_img}/>
+
+        <h1><span>I am Shashika Ekanayaka.</span>I am a computer science 
+            undergraduate in UCSC
+        </h1>
+            <p>I am a experinced web developer and
+                 I'm interseted in AI and ML
+            </p>
+            <div className="hero-action">
+                <div className="hero-connect">Connect with me</div>
+                <div className="hero-resume">My Resume</div>
+            </div>
+
+      </div>
+    
+  )
+}
