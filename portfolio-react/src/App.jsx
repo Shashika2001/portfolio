37626a17d573c17c889dbbe65ab64a97240862c6
Shashika2001 <ekanayakespm@gmail.com>
2024-11-15 20:46:25 +0530

    Start to create Services component

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Services from './components/Services/Services';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
     </div>
   )
 }
---
+++ portfolio-react/src/components/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Services.css';
+
+export default function Services() {
+  return (
+    <div className='services'>
+        <div className="services-title">
+            <h1>Services</h1>
+        </div>
+      
+    </div>
+  )
+}
