26334980f48b1ff189a86081b38eda960a247cd4
Shashika2001 <ekanayakespm@gmail.com>
2024-11-17 15:10:48 +0530

    complte mywork section

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Services from './components/Services/Services';
+import MyWork from './components/MyWork/MyWork';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Hero/>
       <About/>
       <Services/>
+      <MyWork/>
     </div>
   )
 }
