6659d6a0c52409e7f30687147d5d53ee0c20d2c8
Shashika2001 <ekanayakespm@gmail.com>
2024-11-17 21:03:39 +0530

    completed contact page successfully

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Services from './components/Services/Services';
 import MyWork from './components/MyWork/MyWork';
+import Contact from './components/Contact/Contact';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <About/>
       <Services/>
       <MyWork/>
+      <Contact/>
     </div>
   )
 }
