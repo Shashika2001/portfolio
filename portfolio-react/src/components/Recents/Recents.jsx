45440cb27f0ba8f95b3779d5a25ed52243144c47
Shashika2001 <ekanayakespm@gmail.com>
2024-11-23 23:37:46 +0530

    add new section-"Recents"

---
+++ portfolio-react/src/components/Recents/Recents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Recents.css'
+
+export default function Recents() {
+  return (
+    <div className='recents'>
+        <h1>I hope to add here my life events.</h1>
+      
+    </div>
+  )
+}
