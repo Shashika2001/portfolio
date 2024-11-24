72abd48bca3c0f5808550d19194eec66ac927ad9
Shashika2001 <ekanayakespm@gmail.com>
2024-11-24 23:15:52 +0530

    add a small part

--- portfolio-react/src/components/Recents/Recents.jsx
+++ portfolio-react/src/components/Recents/Recents.jsx
@@ -5,6 +5,7 @@ export default function Recents() {
   return (
     <div className='recents'>
         <h1>I hope to add here my life events.</h1>
+        <marquee>Shashika</marquee>
       
     </div>
   )
